67f315cff8338fe411fa90520d47733a5f5f6816
abdallah-lab <72192500+abdallah-lab@users.noreply.github.com>
2021-01-13 07:40:16 +0200

    s

--- Layout/MainLayout.js
+++ Layout/MainLayout.js
@@ -1,16 +1,43 @@
-import MyHead from "../components/MyHead";
-import Header from "../components/Header";
-import NavBar from "../components/NavBar";
-import Footer from "../components/Footer";
+import Head from "../Components/Head";
+import Header from "../Components/Header";
+import NavBar from "../Components/NavBar";
+import Footer from "../Components/Footer";
 
 export default function MainLayout({ title, children }) {
   return (
-    <div>
-      <MyHead title={title} />
-      <Header />
-      <NavBar title={title} />
-      <main>{children}</main>
-      <Footer />
-    </div>
+    <>
+      <Head title={title} />
+      <body>
+        <Header />
+        <NavBar title={title} />
+        <main>{children}</main>
+        <Footer />
+      </body>
+      <style jsx global>
+        {`
+          * {
+            margin: 0;
+            padding: 0;
+            -webkit-box-sizing: border-box;
+            box-sizing: border-box;
+            text-decoration: unset;
+            outline: none;
+            -webkit-tap-highlight-color: transparent;
+            -ms-scroll-chaining: none;
+            overscroll-behavior: contain;
+            -ms-overflow-style: none;
+            scrollbar-width: none;
+          }
+
+          *::-webkit-scrollbar {
+            display: none;
+          }
+          body {
+            width: 100vw;
+            overflow-x: hidden;
+          }
+        `}
+      </style>
+    </>
   );
 }
