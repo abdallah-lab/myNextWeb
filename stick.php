47ff1da2236b276b02185a43eef04e29934cf436
abdallah-lab <72192500+abdallah-lab@users.noreply.github.com>
2021-01-14 15:48:43 +0200

    upload

---
+++ stick.php
@@ -0,0 +1,33 @@
+<div style="height:30px;"></div>
+
+<div id="f_title">My Brands :</div>
+
+<div class="scrollbar" id="style-2">
+     <div class="force-overflow">
+
+<?php $sites = array('../../_mobarak/English/Home','../../_pages/index','../../_pages/ostad') ?>
+<?php for ($x = 1; $x <= 3; $x++){ ?>
+<div>
+<a href="<?php echo $sites[$x-1] ?>"><img height="150px" src="_image/stickers/0_<?php echo $x ?>.png"></div></a>
+<?php } ?>
+
+</div></div>
+
+<div style="height:20px;"></div>
+
+<div id="f_title">I use:</div>
+
+<div class="scrollbar" id="style-2">
+     <div class="force-overflow">
+
+<?php for ($x = 0; $x <= 31; $x++){ ?>
+<div >
+<img  height="150px" src="_image/stickers/<?php echo $x ?>.png"></div>
+<?php } ?>
+
+<div><h1>& much more ...</h1></div>
+
+</div></div>
+
+
+<div style="height:30px"></div>
